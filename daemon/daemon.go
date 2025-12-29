db2bc6a429aef5627cf848c0c48e636a2fe1c4d0
yujizhou <yujizhou@sinosoft.com.cn>
2025-12-29 11:01:49 +0800

    test

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -28,7 +28,7 @@ func (*Daemon) Launch(title, logDir, logFilename string) {
 		if err := os.Chdir("./"); err != nil {
 			panic(err)
 		}
-		// syscall.Umask(0)
+		// syscall.Umask(0) // TODO TEST
 		return
 	}
 
