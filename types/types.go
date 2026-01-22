ae0d0b890a88ad09603913a81feb6ea4324f50bb
yujizhou <yujizhou@sinosoft.com.cn>
2026-01-22 17:38:39 +0800

    [optimize]优化types JSON序列化器

--- types/types.go
+++ types/types.go
@@ -1,16 +1,18 @@
 package types
 
-import "encoding/json"
+import (
+	jsonIter "github.com/json-iterator/go"
+)
 
 // StructToOther 结构体通过 json 转其他格式
 func StructToOther[K any, V any](params K) (ret V, err error) {
 	var b []byte
 
-	if b, err = json.Marshal(params); err != nil {
+	if b, err = jsonIter.Marshal(params); err != nil {
 		return
 	}
 
-	if err = json.Unmarshal(b, &ret); err != nil {
+	if err = jsonIter.Unmarshal(b, &ret); err != nil {
 		return
 	}
 
