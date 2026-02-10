1fa7616af54e43092757b81ec16e1704658c7884
yujizhou <yujizhou@sinosoft.com.cn>
2026-02-10 17:10:29 +0800

    [optimize]validatorV3.readme.md

--- validator/validatorV3/readme.md
+++ validator/validatorV3/readme.md
@@ -228,4 +228,34 @@
     }
     ```
 
+10. 与`gin`框架配合
+    ```go
+    package validatorV3
+    
+    import (
+    	"testing"
+    
+    	"github.com/gin-gonic/gin"
+    )
+    
+    type (
+    	UserRequest struct {
+    		Firstname string `v-rule:"ex:some-ex-check-fn" v-name:"姓"`
+    	}
+    )
+    
+    func Test1(t *testing.T) {
+    	ctx := &gin.Context{}
+    	form, checker := WithGin[UserRequest](ctx, func(form any) (err error) {
+    		// 这里是一个示例的自定义验证函数，可以根据实际需求进行修改
+    		return
+    	})
+    	t.Logf("验证是否通过：%v\n", checker.OK())
+    	for _, wrong := range checker.Wrongs() {
+    		t.Logf("%v\n", wrong)
+    	}
+    	t.Logf("如果验证通过：%v\n", form)
+    }
+    ```
+    
     
--- validator/validatorV3/string_test.go
+++ validator/validatorV3/string_test.go
@@ -1,8 +1,9 @@
 package validatorV3
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/gin-gonic/gin"
 )
 
 type (
@@ -12,26 +13,14 @@ type (
 )
 
 func Test1(t *testing.T) {
-	ur := &UserRequest{
-		Firstname: "张三",
-	}
-
-	validator := APP.Validator.Once()
-
-	checker := validator.Checker(ur)
-	checker.Validate(func(form any) (err error) {
-		// 这里是一次性自定义验证（模拟去数据库中进行验证）
-		if form.(*UserRequest).Firstname != "王五" {
-			err = fmt.Errorf("名字必须是：王五")
-		}
-
+	ctx := &gin.Context{}
+	form, checker := WithGin[UserRequest](ctx, func(form any) (err error) {
+		// 这里是一个示例的自定义验证函数，可以根据实际需求进行修改
 		return
 	})
 	t.Logf("验证是否通过：%v\n", checker.OK())
 	for _, wrong := range checker.Wrongs() {
 		t.Logf("%v\n", wrong)
 	}
-
-	// 验证是否通过：false
-	// 名字必须是：王五
+	t.Logf("如果验证通过：%v\n", form)
 }
