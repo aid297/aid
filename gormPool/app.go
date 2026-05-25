a0f503849d493016f1097e4c9e237f109b9a7105
JerichoYu <yu.jericho.ph@gmail.com>
2026-05-25 16:46:03 +0800

    [optimize]增加mysql默认基础模型

--- gormPool/app.go
+++ gormPool/app.go
@@ -1,9 +1,22 @@
 package gormPool
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 var APP struct {
 	MySQLPool     MySQLPool
 	PGPool        PGPool
 	SQLServerPool SQLServerPool
 	DBSetting     DBSetting
 	Finder        Finder
+}
+
+var MySQLBasic struct {
+	ID        int64          `gorm:"column:id;type:bigint unsigned;primaryKey"`
+	CreatedAt time.Time      `gorm:"column:created_at;type:datetime;not null;default:current_timestamp"`
+	UpdatedAt time.Time      `gorm:"column:updated_at;type:datetime;not null;default:current_timestamp"`
+	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at;type:datetime;not null;default:current_timestamp"`
 }
