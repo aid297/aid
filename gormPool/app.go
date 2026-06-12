ba30825ba149a697cf33f7d940b1f61eccb37290
余济舟 <yujizhou@sinosoft.com.cn>
2026-06-12 15:07:43 +0800

    optimize autoUpdateTime:true

--- gormPool/app.go
+++ gormPool/app.go
@@ -13,7 +13,7 @@ var APP struct {
 type MySQLBasic struct {
 	ID        int64          `gorm:"column:id;type:bigint unsigned;primaryKey" json:"id"`
 	CreatedAt time.Time      `gorm:"column:created_at;type:datetime;not null;default:current_timestamp" json:"created_at"`
-	UpdatedAt time.Time      `gorm:"column:updated_at;type:datetime;not null;default:current_timestamp" json:"updated_at"`
+	UpdatedAt time.Time      `gorm:"column:updated_at;type:datetime;not null;default:current_timestamp;autoUpdateTime:true" json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at;type:datetime;not null" json:"deleted_at"`
 	UUID      string         `gorm:"column:uuid;type:char(36);not null;unique" json:"uuid"`
 }
