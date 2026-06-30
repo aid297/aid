d64461c5a0e415688fa8440e1cf54d3895d02ea7
余济舟 <yujizhou@sinosoft.com.cn>
2026-06-30 16:20:03 +0800

    optimize:types.StructToOther方法改为ToAny,改为一个泛型参数

--- types/types.go
+++ types/types.go
@@ -1,20 +1,16 @@
 package types
 
 import (
-	jsonIter "github.com/json-iterator/go"
+	"github.com/bytedance/sonic"
 )
 
-// StructToOther 结构体通过 json 转其他格式
-func StructToOther[K any, V any](params K) (ret V, err error) {
+// ToAny 转换到任意格式
+func ToAny[DST any](src any, dst *DST) (err error) {
 	var b []byte
 
-	if b, err = jsonIter.Marshal(params); err != nil {
+	if b, err = sonic.Marshal(src); err != nil {
 		return
 	}
 
-	if err = jsonIter.Unmarshal(b, &ret); err != nil {
-		return
-	}
-
-	return
+	return sonic.Unmarshal(b, &dst)
 }
